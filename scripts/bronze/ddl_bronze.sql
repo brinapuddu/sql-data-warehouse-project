07215dd8a78538806303267a70b5519fcc3a181d
brinapuddu <96079402+brinapuddu@users.noreply.github.com>
2026-03-26 22:06:11 +0100

    Drop and create crm_cust_info table in bronze schema

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -7,6 +7,7 @@
 -- CRM Tables
 -- ------------------------------------------------------------
 */
+
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
