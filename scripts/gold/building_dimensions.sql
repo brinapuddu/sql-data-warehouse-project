181a563d0d2f1ba941b167c5ad8ddff36749cb0e
brinapuddu <96079402+brinapuddu@users.noreply.github.com>
2026-03-30 15:21:50 +0200

    Add SQL script for building customer dimensions view
    Create a view for customer dimensions with gender and other details.

---
+++ scripts/gold/building_dimensions.sql
@@ -0,0 +1,31 @@
+--Data Integration 
+SELECT 
+ci.cst_gndr,
+ca.gen,
+CASE WHEN ci.cst_gndr != 'n/a' THEN  ci.cst_gndr --CRM is the Master for gender info
+	ELSE COALESCE(ca.gen, 'n/a')
+END AS new_gender
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+	ON ci.cst_key = ca.cid
+ORDER BY 1,2
+
+CREATE VIEW gold.dim_customers AS
+SELECT 
+ROW_NUMBER() OVER(ORDER BY ci.cst_id) AS customer_key,
+ci.cst_id AS customer_id,
+ci.cst_key AS customer_number,
+ci.cst_firstname AS first_name,
+ci.cst_lastname AS last_name,
+la.cntry as country,
+ci.cst_marital_status AS marital_status,
+CASE WHEN ci.cst_gndr != 'n/a' THEN  ci.cst_gndr --CRM is the Master for gender info
+	ELSE COALESCE(ca.gen, 'n/a')
+END AS gender,
+ci.cst_create_date AS create_date, 
+ca.bdate as birthdate
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+	ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+	ON ci.cst_key = la.cid
