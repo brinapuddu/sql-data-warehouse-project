4e01f0a3bb9fdf8d18b45404b1dc01f4e555e3bf
brinapuddu <96079402+brinapuddu@users.noreply.github.com>
2026-03-30 13:38:54 +0200

    Update and rename Data_Cleaning_erp_just_az12.sql to Data_Cleaning_erp_cust_az12.sql

--- scripts/silver/Data_Cleaning_erp_cust_az12.sql
+++ scripts/silver/Data_Cleaning_erp_cust_az12.sql
@@ -5,23 +5,23 @@ Data Cleaning
 Purpose:
     Process and clean data from the Bronze layer of the data warehouse into the Silver layer. 
 Focus:
-    - ERP : erp_just_az12
+    - ERP : erp_cust_az12
 ================================================================================
 */
 
 --Identify out of range birth dates
 SELECT DISTINCT
 bdate
-FROM bronze.erp_just_az12
+FROM bronze.erp_cust_az12
 WHERE bdate < '1926-01-01' OR bdate > GETDATE();
 
 --Data Standardization & Consistency
 SELECT
 gen
-FROM bronze.erp_just_az12;
+FROM bronze.erp_cust_az12;
 
 
-INSERT INTO silver.erp_just_az12 (
+INSERT INTO silver.erp_cust_az12 (
 	cid,
 	bdate,
 	gen
@@ -37,9 +37,9 @@ CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
      WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
 	ELSE 'n/a'
 END AS gen
-FROM bronze.erp_just_az12
+FROM bronze.erp_cust_az12
 
 --Final Check
 SELECT*
 FROM
-silver.erp_just_az12
+silver.erp_cust_az12
