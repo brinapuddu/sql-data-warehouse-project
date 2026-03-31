a22043b6f2048038a5886152ccd2ed69a69fc20d
brinapuddu <96079402+brinapuddu@users.noreply.github.com>
2026-03-31 17:38:30 +0200

    Update build_dim_prod.sql

--- scripts/gold/build_dim_prod.sql
+++ scripts/gold/build_dim_prod.sql
@@ -28,7 +28,7 @@ SELECT
 ROW_NUMBER() OVER(ORDER BY prd_start_dt,prd_key) AS product_key, 
 	pn.prd_id AS product_id,
 	pn.prd_nm AS product_name,
-	pn.prd_key AS produt_number,
+	pn.prd_key AS product_number,
 	pn.prd_cost AS product_cost, 
 	pn.prd_line AS product_line, 
 	pn.prd_start_dt AS start_date, 
